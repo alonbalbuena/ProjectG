81bdd04a2981e716c400f1f84902c11c6373123a
alonbalbuena <alonbalbuena@gmail.com>
2023-08-14 22:42:23 +0200

    feat: init repository

---
+++ backend/src/main/java/com/projectg/projectg/ProjectgApplication.java
@@ -0,0 +1,13 @@
+package com.projectg.projectg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectgApplication.class, args);
+	}
+
+}

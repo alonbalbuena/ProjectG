b05eca98a2d052ed8dc1158e5e8c5e802ed7d604
alogarcia <alogarcia@aidacanarias.com>
2023-09-14 02:20:10 +0200

    feat: add gh-pages deploy automatization

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: '/ProjectG/', // baseURL: '/<repository>/'
+    buildAssetsDir: 'assets', // don't use "_" at the begining of the folder name to avoids nojkill conflict
+  },
   devtools: { enabled: true },
   css: [
     "primevue/resources/themes/mira/theme.css",
@@ -10,4 +14,5 @@ export default defineNuxtConfig({
     transpile: ["primevue"],
   },
   modules: ["@pinia/nuxt"],
+
 });
